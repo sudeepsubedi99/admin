8459dae04240f0ee9d9b0615c940cb568e6b4338
sudeepsubedi99 <sudeepsubedi9961@gmail.com>
2020-11-05 15:10:10 +0545

    adminlte added

--- config/app.php
+++ config/app.php
@@ -1,5 +1,8 @@
 <?php
 
+
+
+
 return [
 
     /*
@@ -165,6 +168,7 @@ return [
         /*
          * Package Service Providers...
          */
+        App\Providers\FortifyServiceProvider::class,
 
         /*
          * Application Service Providers...
